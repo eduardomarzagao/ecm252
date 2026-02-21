ab412179592044b9336e2bb2a5d2114f0130a3f0
Eduardo Marzagao <eduardo.marzagao@hotmail.com>
2026-02-21 12:36:46 -0300

    fix: Correção do arquivo de testes

--- ap01_git_introducao/teste_calculadora.py
+++ ap01_git_introducao/teste_calculadora.py
@@ -13,19 +13,19 @@ while (escolha != -1):
         case 1:
             print("Somar: Digite os números separados por ','")
             numeros = [int(n) for n in input().split(', ')]
-            print(f"Resultado: {somarList(numeros)}")
+            print(f"Resultado: {calculadora.somarList(numeros)}")
             print("")
         case 2:
             print("Subtrair: Digite os números separados por ','")
             numeros = [int(n) for n in input().split(', ')]
-            print(f"Resultado: {subtrairList(numeros)}")
+            print(f"Resultado: {calculadora.subtrairList(numeros)}")
             print("")
         case 3:
             print("Multiplicar: Digite o primeiro número: ")
             a = int(input())
             print("Digite o segundo número: ")
             b = int(input())
-            print(f"Resultado: {multiplicar(a, b)}")
+            print(f"Resultado: {calculadora.multiplicar(a, b)}")
             print("")
         case 4:
             print("Dividir: Digite o primeiro número: ")
@@ -35,7 +35,7 @@ while (escolha != -1):
             if(b == 0):
                 print("Não é possível dividir por 0!\n")
                 continue
-            print(f"Resultado: {dividir(a, b)}")
+            print(f"Resultado: {calculadora.dividir(a, b)}")
             print("")
         case default:
             print("Escolha uma opção válida!!!\n")
